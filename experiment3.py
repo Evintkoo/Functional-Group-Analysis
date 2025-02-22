a4d88e5ff75af4af4a14c70a6960aa5f31babbce
Evintkoo <99308658+Evintkoo@users.noreply.github.com>
2025-02-22 11:29:48 -0800

    update analysis

--- experiment3.py
+++
@@ -1,9 +0,0 @@
-from experiment3_autoencoder_train import run_train
-from experiment3_som import train_som
-
-def main():
-    run_train()
-    train_som()
-    
-if __name__ == "__main__":
-    main()
